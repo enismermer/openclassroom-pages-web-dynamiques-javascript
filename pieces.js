e418748f3e90af83d8ccff2c824e8db4c83cfba5
enis <enismermer38@gmail.com>
2024-11-23 14:28:52 +0100

    manipuler les listes en js

--- pieces.js
+++ pieces.js
@@ -2,36 +2,87 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
+for (let i = 0; i < pieces.length; i++) {
 
-// Création des balises 
-const article = pieces[0];
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+    
+}
+// Gestion des boutons
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+// Trier la liste dans l'ordre croissant
+const boutonTrier = document.querySelector(".btn-trier");
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
 
-// Création des éléments pour l'exercice
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
-
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    console.log(piecesOrdonnees);
+});
 
 
-//Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
+// Filtrer la liste avec le prix inférieur ou égal à 35 euros
+const boutonFiltrer = document.querySelector(".btn-filtrer");
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+boutonFiltrer.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+    
+    console.log(piecesFiltrees);
+    
+});
+
+// Trier la liste dans l'ordre décroissant
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+    const piecesDesordonnees = Array.from(pieces);
+    
+    piecesDesordonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(piecesDesordonnees);
+});
+
+// Filtrer la liste sans la description
+const boutonNoDescription = document.querySelector(".btn-nodescription");
+
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description;
+    })
+    console.log(piecesFiltrees);
+})
+
